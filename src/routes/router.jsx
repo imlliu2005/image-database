941a39a616249d74b41b5d0c26384b139c0439eb
ningliu <ningliu@keyayun.com>
2019-05-19 11:17:58 +0800

    add router

---
+++ src/routes/router.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Home from "../components/home";
+import About from "../components/about";
+import Topics from "../components/topics";
+
+const BasicExample = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/topics">Topics</Link>
+        </li>
+      </ul>
+
+      <hr />
+
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/topics" component={Topics} />
+    </div>
+  );
+};
+
+export default BasicExample;
